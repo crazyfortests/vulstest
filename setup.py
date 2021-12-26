854dabcb3771a2ba3d673216c49961ef8b99e833
crazyfortests <68438186+crazyfortests@users.noreply.github.com>
2021-12-26 22:17:12 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,22 @@
+# https://medium.com/@joel.barmettler/how-to-upload-your-python-package-to-pypi-65edc5fe9c56
+"""Setup for the clrprint package."""
+
+#from distutils.core import setup
+from setuptools import setup, Extension
+
+setup(
+    author="crazy",
+    name='vulstest',
+    version='v0.0.1',
+    download_url='https://github.com/crazyfortests/vulstest/archive/refs/tags/v0.0.1.tar.gz',
+    python_requires=">=3.2",
+    classifiers=[
+        # Trove classifiers
+        # (https://pypi.python.org/pypi?%3Aaction=list_classifiers)
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Software Development :: Build Tools',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+    ],
+)
---
+++ vulstest/vulstest.py
@@ -0,0 +1,13 @@
+import sys
+
+version = '0.0.1'
+by='crazy'
+def fun1(): 
+    print('fun1' , version, by )
+
+
+def fun2():
+    print('fun2',version, by)
+
+def fun3():
+    print('fun3', version, by)
